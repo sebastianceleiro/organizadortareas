e9d9abe75efddd6606314653387903662930b5cc
Sebastián <87915563+sebastianceleiro@users.noreply.github.com>
2021-09-01 20:08:37 -0300

    3er entrega proyecto

--- js/codigo.js
+++ js/codigo.js
@@ -8,22 +8,20 @@ class Tarea {
         this.detalle = detalle;
         this.estado = estado;
         this.prioridad = prioridad;
-        this.asignacion = asignacion ;
-        this.fecha = fecha ;
+        this.asignacion = asignacion;
+        this.fecha = fecha;
     }
-
     ingresoDeDatos()  {
 
      
         this.id = totalTareas.length   ;
-        this.nombre = $("#Tarea").val() ;
-        this.detalle = $("#Descripcion").val() ;
-        this.estado = $("#Estado").val() ;
-        this.prioridad = $("#Prioridad").val() ;
-        this.asignacion = $("#Asignacion").val() ;
+        this.nombre = $("#tarea").val() ;
+        this.detalle = $("#descripcion").val() ;
+        this.estado = $("#estado").val() ;
+        this.prioridad = $("#prioridad").val() ;
+        this.asignacion = $("#asignacion").val() ;
         this.fecha = new Date ().toLocaleDateString(); ;
     } 
-
 }
 
 //                                  VARIABLES
@@ -33,15 +31,28 @@ let comprobarTareas ;
 let resultadoFiltro = [] ;
 let modoEdicion = false ;
 let idAEditar ;
+const usuarios = "data/usuarios.json" ;
 
 
+function lecturaUsuarios () {
+
+    $.getJSON(usuarios, function(respuesta,estado) { 
+        if (estado === "success") {
+            let listadoUsuarios = respuesta;
+            console.log (listadoUsuarios);
+            for (let usuario of listadoUsuarios) {
+            $("#asignacion").append("<option>" + usuario.nombre + "</option>");
+            };
+        };
+    }); 
+} 
 //                                 FUNCIONES 
 
 
 function  armadoTablaSuperior () 
 {
-    $("#Tabladetareas").empty(); 
-    $("#Tabladetareas").append ("<tr id='cabecera'> " +
+    $("#tablaDeTareas").empty(); 
+    $("#tablaDeTareas").append ("<tr id='cabecera'> " +
     "<td>ID </td> " +
     "<td>Tarea</td>" +
     "<td>Detalle</td>" +
@@ -61,14 +72,14 @@ function mostrarTareaIngresada () {
    for (let tarea of totalTareas) { 
 
    
-       $("#Tabladetareas").append ('<tr class="tarea"> <td>'  + tarea.id + 
+       $("#tablaDeTareas").append ('<tr class="tarea"> <td>'  + (tarea.id+1) + 
        " <td>" + tarea.nombre +
        " <td>" + tarea.detalle +
        " <td>" + tarea.estado + 
        " <td>" + tarea.asignacion +
        " <td>" + tarea.prioridad +
        " <td>" + tarea.fecha +
-       ' <td> <button class="botonEliminar" id=id> Eliminar </button> <button class="botonEditar" id=id2 >Editar</button>' ) ;
+       ' <td> <button class="botonEliminar btn btn-success btn-sm" id=id>Eliminar</button> <button class="botonEditar btn btn-success btn-sm" id=id2>Editar</button>' ) ;
 
        $("#id").attr("id", tarea.id) ;
        $("#id2").attr("id", tarea.id) ;
@@ -81,137 +92,127 @@ function mostrarTareaIngresada () {
 
 // Se borran los datos de los formularios al guardar tarea
 function borrarValues () {
-    $("#Tarea").val("")   ;
-    $("#Descripcion").val("") ;
-
+    $("#tarea").val("")   ;
+    $("#descripcion").val("") ;
 }
 
 function agregarTareas(e) { 
    
     e.preventDefault() ;
-    if ($("#Tarea").val() != "" & (modoEdicion == false)) /* valida que el campo tenga algo*/ {
+    if ($("#tarea").val() != "" & (modoEdicion == false)) /* valida que el campo tenga algo*/ {
       
-        datosUsuario = new Tarea () ;  // Se crea el objeto con las propiedades que componen cada ingreso de tarea
-        datosUsuario.ingresoDeDatos() ;   // Se solicita el ingreso de datos que quedaran en el objeto
-        totalTareas.push (datosUsuario) ;  // Se guarda el objeto en un array que contiene todas las tareas
-        mostrarTareaIngresada() ; // Se muestra todo el listado de tareas ingresado 
+        datosUsuario = new Tarea ();  // Se crea el objeto con las propiedades que componen cada ingreso de tarea
+        datosUsuario.ingresoDeDatos();   // Se solicita el ingreso de datos que quedaran en el objeto
+        totalTareas.push (datosUsuario);  // Se guarda el objeto en un array que contiene todas las tareas
+        mostrarTareaIngresada(); // Se muestra todo el listado de tareas ingresado 
         borrarValues ();
-    let tareasJson = JSON.stringify(totalTareas) ; // Converite objeto a JSON
-    localStorage.setItem ("Tareas", tareasJson) ;  // Guardo en LS  el JSON
-}   else if ($("#Tarea").val() == "" ) { alert ("Por favor ingrese el nombre de la tarea" );  } ;
+    let tareasJson = JSON.stringify(totalTareas); // Converite objeto a JSON
+    localStorage.setItem ("tareas", tareasJson);  // Guardo en LS  el JSON
+}   else if ($("#tarea").val() == "" ) { alert ("Por favor ingrese el nombre de la tarea" );  } ;
     if (modoEdicion == true) // Se reemplaza elemento si se edita
     {
-        totalTareas[idAEditar].nombre = $("#Tarea").val() ;
-        totalTareas[idAEditar].detalle = $("#Descripcion").val() ;
-        totalTareas[idAEditar].estado = $("#Estado").val() ;
-        totalTareas[idAEditar].prioridad = $("#Prioridad").val() ;
-        totalTareas[idAEditar].asignacion = $("#Asignacion").val() ;
-        mostrarTareaIngresada() ; // Se muestra todo el listado de tareas ingresado 
+        totalTareas[idAEditar].nombre = $("#tarea").val();
+        totalTareas[idAEditar].detalle = $("#descripcion").val();
+        totalTareas[idAEditar].estado = $("#estado").val();
+        totalTareas[idAEditar].prioridad = $("#prioridad").val();
+        totalTareas[idAEditar].asignacion = $("#asignacion").val();
+        mostrarTareaIngresada(); // Se muestra todo el listado de tareas ingresado 
         borrarValues ();
-        modoEdicion == false ;
-        let tareasJson = JSON.stringify(totalTareas) ; // Converite objeto a JSON
-        localStorage.setItem ("Tareas", tareasJson) ;  // Guardo en LS  el JSO
-        $("#tituloFormulario").text("Nuevo ingreso") ;
-        
-    }
+        modoEdicion == false;
+        let tareasJson = JSON.stringify(totalTareas); // Converite objeto a JSON
+        localStorage.setItem ("tareas", tareasJson);  // Guardo en LS  el JSO
+        $("#tituloFormulario").text("Nuevo ingreso");
+        }
 }
 
 
 function comprobarSiHayTareas()  {
-    let comprobarTareas = JSON.parse(localStorage.getItem("Tareas"));
-    if (comprobarTareas != null)
-        {
-        totalTareas = comprobarTareas ;
-        mostrarTareaIngresada () ;
-  
-            }
-
+    let comprobarTareas = JSON.parse(localStorage.getItem("tareas"));
+    if (comprobarTareas != null) {
+        totalTareas = comprobarTareas;
+        mostrarTareaIngresada ();
+  }
 }
 
 
 function eliminar () {
     let confirmar = confirm ("Por favor confirmar si desea eliminar TODAS las tareas") ;
     if (confirmar == true ) {
-        totalTareas = [] ;
-        tareasJson = JSON.stringify(totalTareas) ; // Converite objeto a JSON
-        localStorage.setItem ("Tareas", tareasJson) ;  // Guardo en LS  el JSON
-        mostrarTareaIngresada () ;
+        totalTareas = [];
+        tareasJson = JSON.stringify(totalTareas); // Converite objeto a JSON
+        localStorage.setItem ("tareas", tareasJson);  // Guardo en LS  el JSON
+        mostrarTareaIngresada ();
        }
-}  ;
+};
 
 function eliminarUna(e) {
     console.log(e.target.id) ;
-    totalTareas.splice (e.target.id, 1) ;
-    let contador = 0 ;
+    totalTareas.splice (e.target.id, 1);
+    let contador = 0;
     for (let tarea of totalTareas)  // Actualiza los ID si algo se borra
         {   
-            tarea.id = contador ;   ;
-            contador++  ;} ;
+            tarea.id = contador;   
+            contador++  ;};
 
     tareasJson = JSON.stringify(totalTareas) ; // Converite objeto a JSON
-    localStorage.setItem ("Tareas", tareasJson) ;  // Guardo en LS  el JSON
-    mostrarTareaIngresada () ;
+    localStorage.setItem ("tareas", tareasJson) ;  // Guardo en LS  el JSON
+    mostrarTareaIngresada ();
 } ;
 
 
 function editarTarea(e) {
   
-    $("#Tarea").val(totalTareas[e.target.id].nombre)   ;
-    $("#Descripcion").val(totalTareas[e.target.id].detalle)   ;
-    $("#Estado").val(totalTareas[e.target.id].estado)   ;
-    $("#Asignacion").val(totalTareas[e.target.id].asignacion)   ;
-    $("#Prioridad").val(totalTareas[e.target.id].prioridad)   ;
+    $("#tarea").val(totalTareas[e.target.id].nombre);
+    $("#descripcion").val(totalTareas[e.target.id].detalle);
+    $("#estado").val(totalTareas[e.target.id].estado);
+    $("#asignacion").val(totalTareas[e.target.id].asignacion);
+    $("#prioridad").val(totalTareas[e.target.id].prioridad);
 
     modoEdicion = true ;
     idAEditar = e.target.id ;
-    $("#tituloFormulario").text("Modificar Tarea") ;
-
-   
-
+    $("#tituloFormulario").text("Modificar Tarea");
 }
 
 // Funciones que se ejecutan cuando el usuario hace click
 
-$("#formulario").submit(agregarTareas) ; 
-$("#eliminar").click(eliminar) ;
-$("#filtrar").click(filtrarTareas) ;
-$(".botonEliminar").click(eliminarUna) ; // Esta linea no funciona
-$("#ocultar").click(ocultarIngresos) ;
-$(".botonEditar").click(editarTarea) ;
-
+$("#formulario").submit(agregarTareas); 
+$("#eliminar").click(eliminar);
+$("#filtrar").click(filtrarTareas);
+$(".botonEliminar").click(eliminarUna); // Esta linea no funciona
+$("#ocultar").click(ocultarIngresos);
+$(".botonEditar").click(editarTarea);
 
 /// FILTRADO
-
 
 function mostrarFiltrado () {
     armadoTablaSuperior () 
 for (let tarea of resultadoFiltro) { 
 
-    $("#Tabladetareas").append ('<tr class="tarea"> <td>'  + tarea.id + 
-    " <td>" + tarea.nombre +
-    " <td>" + tarea.detalle +
-    " <td>" + tarea.estado + 
-    " <td>" + tarea.asignacion +
-    " <td>" + tarea.prioridad +
-    " <td>" + tarea.fecha +
-    ' <td> <button class="botonEliminar" id=id> Eliminar  </button>' ) ;
+    $("#tablaDeTareas").append ('<tr class="tarea"> <td>'  + (tarea.id+1) + 
+       " <td>"  + tarea.nombre  +
+       " <td>" + tarea.detalle +
+       " <td>" + tarea.estado + 
+       " <td>" + tarea.asignacion +
+       " <td>" + tarea.prioridad +
+       " <td>" + tarea.fecha +
+       ' <td> <button class="botonEliminar btn btn-success btn-sm" id=id>Eliminar</button> <button class="botonEditar btn btn-success btn-sm" id=id2>Editar</button>' ) ;
 
-        $("#id").attr("id", tarea.id) ;
+       $("#id").attr("id", tarea.id);
+       $("#id2").attr("id", tarea.id);
      
-} ;      $(".botonEliminar").click(eliminarUna) ; }
+} ;      $(".botonEliminar").click(eliminarUna); }
 
 
 function filtrarTareas () {
     resultadoFiltro = totalTareas.filter ( pendientes => pendientes.estado == "Pendiente") ;
     mostrarFiltrado ();
-}  ;
+};
 
 function ocultarIngresos() {
-
    $("#formulario").slideToggle(1000) ;
- 
-   }
+  }
  
 
 ///Se comprueba al cargar pagina si hay tareas en el Local Storage
-comprobarSiHayTareas() ;
+comprobarSiHayTareas();
+lecturaUsuarios();
